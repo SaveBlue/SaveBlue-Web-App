a7eab30c5928e515d2b13716c00cdb2e65d7f70e
Matej Horvat <matej@horvat.si>
2020-09-14 20:50:12 +0200

    Update account card

--- src/frontend/src/components/AccountDashboardCard.vue
+++ src/frontend/src/components/AccountDashboardCard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="account-dashboard-card mdl-card mdl-shadow--2dp mdl-cell
-       mdl-cell--6-col-desktop  ">
+  <div v-on:click="cardClick" class="account-dashboard-card mdl-card mdl-shadow--2dp mdl-cell
+       mdl-cell--6-col-desktop">
     <div class="mdl-card__title">
       <h2 class="mdl-card__title-text">Account Name</h2>
     </div>
@@ -11,18 +11,17 @@
         Account Balance: €€€
       </h5>
     </div>
-
-    <div class="mdl-card__menu">
-      <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-        <i class="material-icons">edit</i>
-      </button>
-    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "AccountDashboardCard"
+  name: "AccountDashboardCard",
+  methods:{
+    cardClick: function (){
+      alert('clicked')
+    }
+  }
 }
 </script>
 
